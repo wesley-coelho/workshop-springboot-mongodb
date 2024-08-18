5288f2686d5b353f72bd7303bff53e85d5d549ed
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-17 21:48:25 -0300

    Conectando ao mongodb com repository e service

---
+++ src/main/java/com/wesleycoelho/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.wesleycoelho.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesleycoelho.workshopmongo.domain.User;
+import com.wesleycoelho.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
