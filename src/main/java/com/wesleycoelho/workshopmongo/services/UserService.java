7926ede8fa7f2defe37ce57839aac4a2aec309d5
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-19 20:10:22 -0300

    Obtendo um usuario por id

--- src/main/java/com/wesleycoelho/workshopmongo/services/UserService.java
+++ src/main/java/com/wesleycoelho/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.wesleycoelho.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wesleycoelho.workshopmongo.domain.User;
 import com.wesleycoelho.workshopmongo.repository.UserRepository;
+import com.wesleycoelho.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+	    Optional<User> user = repo.findById(id);
+		if(user.isEmpty() ) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		return user.get();
 	}
 }
