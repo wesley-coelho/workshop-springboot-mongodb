f88f5550ba6d293468810e6f4f9ce4caba495058
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-19 22:10:04 -0300

    Consulta simples com query methods

--- src/main/java/com/wesleycoelho/workshopmongo/services/PostService.java
+++ src/main/java/com/wesleycoelho/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.wesleycoelho.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wesleycoelho.workshopmongo.domain.Post;
-import com.wesleycoelho.workshopmongo.domain.User;
 import com.wesleycoelho.workshopmongo.repository.PostRepository;
 import com.wesleycoelho.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +23,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Objeto não encontrado!");
 		}
 		return post.get();
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 	
