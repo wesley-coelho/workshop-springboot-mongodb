6faf3f92b8c23e8bc6104f127094bc7147bdc73a
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-19 21:36:53 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/wesleycoelho/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wesleycoelho/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.wesleycoelho.workshopmongo.domain.Post;
 import com.wesleycoelho.workshopmongo.domain.User;
 import com.wesleycoelho.workshopmongo.dto.UserDTO;
 import com.wesleycoelho.workshopmongo.services.UserService;
@@ -58,6 +59,15 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);		
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 	
 	
 	
